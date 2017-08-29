1a8cbd67c98b634f489f7bdd7f87124e2f51a0d9
richard <ruhuayan@gmail.com>
2017-08-29 16:23:38 -0400

    Editor

---
+++ assets/global/plugins/ckeditor/config.js
@@ -0,0 +1,30 @@
+/**
+ * @license Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	config.language = 'en';
+  //  config.uiColor = '#F7B42C';
+  config.height = 100;
+  config.line_height="normal !important ";
+  config.toolbarCanCollapse = true;
+ //config.removePlugins='elementspath',
+ config.extraAllowedContent = 'video[*]{*};source[*]{*}';
+
+config.toolbarGroups = [
+		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+		{ name: 'forms', groups: [ 'forms' ] },
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+		{ name: 'links', groups: [ 'links' ] },
+		{ name: 'insert', groups: [ 'insert' ] },
+		{ name: 'styles', groups: [ 'styles' ] },
+		{ name: 'colors', groups: [ 'colors' ] },
+		{ name: 'tools', groups: [ 'tools' ] },
+		{ name: 'others', groups: [ 'others' ] },
+		{ name: 'about', groups: [ 'about' ] }
+	];
+};
