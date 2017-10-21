56ba19b63330aa2a9dea92b0a8d270d8c2923bf1
richard <ruhuayan@gmail.com>
2017-10-21 17:22:33 -0400

    fixed download

--- php/login.php
+++
@@ -1,5 +0,0 @@
-<?php
-$userName = filter_input(INPUT_POST, 'userName', FILTER_SANITIZE_STRING);
-$pass = filter_input(INPUT_POST, 'pass', FILTER_SANITIZE_STRING);
-
-if($userName) echo json_encode(array('userName'=>$userName));
